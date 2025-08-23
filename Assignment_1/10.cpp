455a169420ede16083f09911ef8c4e94521dfaac
Sinan Sevgi <sinan.sevgi@gmail.com>
2025-08-23 13:09:48 +0300

    Assignment 1 Question 10

---
+++ Assignment_1/10.cpp
@@ -0,0 +1,69 @@
+/*
+Problem Statement:
+Print the following pattern but coming up with a code logic. Keep the
+program modular, i.e. The pattern should be print for a given input x.
+
+**********
+****  ****
+***    ***
+**      **
+*        *
+**      **
+***    ***
+****  ****
+**********
+
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void linePrinter(int lines)
+{
+    for (int i = 0; i<2*lines-1; i++)
+    {
+        for ( int j = 0; j<lines; j++)
+        {
+            if(j < lines-i)
+            {
+                cout << '*';
+            }
+            else if (lines - i <= 0 && j <= -1*(lines-i)+1)
+            {
+                cout << '*';
+            }
+            else
+            {
+                cout << ' ';
+            }
+        }
+        for ( int j = lines-1; j>=0; j--)
+        {
+            if(lines-j > i)
+            {
+                cout << '*';
+            }
+            else if (lines - i <= 0 && j <= -1*(lines-i)+1)
+            {
+                cout << '*';
+            }
+            else
+            {
+                cout << ' ';
+            }
+        }
+        cout << endl;
+    }
+
+
+}
+
+int main()
+{
+    int lines = 0;
+    cout << "Please enter amount of lines you want to :" << endl;
+    cin >> lines;
+    cout << endl;
+    linePrinter(lines);
+
+}
